5aed8dba97c61df9b94bcfc3e78d9f6a9c3eea57
boodac <180095661+Boodac@users.noreply.github.com>
2024-12-20 22:19:11 -0500

    fear: add search for a location and retrieve weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-let x;
+import { reset } from "./assets";
+
+// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=M2LN6SAFB33F6JKH24YB45ASD
+
+const schema = {
+    path: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/",
+    key: "?key=M2LN6SAFB33F6JKH24YB45ASD"
+};
+
+async function getWeatherData(place) {
+    const response = await fetch(`${schema.path}/${place}/${schema.key}`);
+    if(response.status === 200) return response.json();
+    else return response;
+}
+
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+
+button.addEventListener("click", e => {
+    getWeatherData(input.value).then(response => {
+        console.log(response);
+    }).catch(e=>console.log(e));
+});
