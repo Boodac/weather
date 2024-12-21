fe30bfc4d27343cc88e7b95c53d2c07178e5c47d
boodac <180095661+Boodac@users.noreply.github.com>
2024-12-21 18:37:36 -0500

    add functionality to display results of query on page. handle invalid data gracefully. also add key handling for enter key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import { reset } from "./assets";
+import { reset, styles } from "./assets";
 
 // https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=M2LN6SAFB33F6JKH24YB45ASD
+
+
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const content = document.querySelector("#content");
 
 const schema = {
     path: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/",
@@ -10,14 +15,44 @@ const schema = {
 async function getWeatherData(place) {
     const response = await fetch(`${schema.path}/${place}/${schema.key}`);
     if(response.status === 200) return response.json();
-    else return response;
-}
+    else throw new Error("invalid query");
+};
 
-const button = document.querySelector("button");
-const input = document.querySelector("input");
+button.addEventListener("click", handleInput);
+input.addEventListener('keyup', e => {
+    if(e.key === "Enter") {
+        handleInput(e);
+    };
+})
 
-button.addEventListener("click", e => {
+function handleInput(e) {
+    content.textContent = "";
     getWeatherData(input.value).then(response => {
         console.log(response);
-    }).catch(e=>console.log(e));
-});
+        processData(response);
+    }).catch(e=> {
+        console.log(e);
+        const invalidText = document.createElement("p");
+        invalidText.textContent = "Invalid Query.";
+        content.appendChild(invalidText);
+    });
+}
+
+function processData(response) {
+    const currentConditions = response.currentConditions;
+    currentConditions.resolvedAddress = response.resolvedAddress;
+    createConditionsTable(currentConditions);
+}
+
+function createConditionsTable(currentConditions) {
+    const template = document.querySelector("#currentConditions");
+    const clone = template.content.cloneNode(true);
+    console.log(clone);
+    for(const [key, value] of Object.entries(currentConditions)) {
+        if(clone.querySelector(`#${key}`)) {
+            if(value) clone.querySelector(`#${key}`).textContent = value;
+            else clone.querySelector(`#${key}`).textContent = "None";
+        }
+    };
+    content.appendChild(clone);
+}
